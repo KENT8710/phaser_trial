9a61a9dd3c2b80323b63f5b1445b054ca337d998
KENT8710 <matsui.kento8@gmail.com>
2018-08-05 09:37:09 +0900

    1st commit

--- app/javascript/dragon/cave/preload.js
+++ app/javascript/dragon/cave/preload.js
@@ -1,5 +1,5 @@
 import TilesSprite from "../../images/tiles-sprite.png"
-import PlayerSprite from "../../images/player-sprite.png"
+import PlayerSprite from "../../images/player-sprite2.png"
 
 export default function preload() {
   this.load.tilemapTiledJSON("cave_map", "/dragon/cave_map.json?t=" + new Date())
@@ -7,6 +7,13 @@ export default function preload() {
     frameWidth: 64, frameHeight: 64
   })
   this.load.spritesheet("player", PlayerSprite, {
-    frameWidth: 64, frameHeight: 64
+    frameWidth: 48, frameHeight: 64
   })
-}
+}export const BACK_1 = 0
+export const BACK_2 = 2
+export const RIGHT_1 = 3
+export const RIGHT_2 = 5
+export const FRONT_1 = 6
+export const FRONT_2 = 8
+export const LEFT_1 = 9
+export const LEFT_2 = 11
